263a14116ab43e73ee960ac7e17c2a6498920047
deceptible <mitchellfreitas19@gmail.com>
2026-05-10 19:29:16 -0400

    Piece Update

--- Piece.py
+++ Piece.py
@@ -1,63 +1,61 @@
 import numpy as np
 
 class Piece:
+    def Transpose(self):
+        return np.transpose(self.matrix)
+
     def Rotate(self):
-        pass
+        transposed = self.Transpose()
 
-class I:
+        self.matrix = np.fliplr(transposed)
+    
+    def getMatrix(self):
+        return self.matrix
+
+class I(Piece):
     def __init__(self):
-        self.grid = np.array([
-            [0, 0, 0, 0], 
-            [0, 0, 0, 0], 
-            [0, 0, 0, 0], 
+        self.matrix = np.array([
             [1, 1, 1, 1]
-            ])
+        ])
 
-class O:
+class O(Piece):
     def __init__(self):
-        self.grid = np.array([
-            [0, 0, 0, 0], 
-            [0, 0, 0, 0], 
-            [0, 1, 1, 0], 
-            [0, 1, 1, 0]
-            ])
-class T:
+        self.matrix = np.array([
+            [1, 1], 
+            [1, 1]
+        ])
+
+class T(Piece):
     def __init__(self):
-        self.grid = np.array([
-            [0, 0, 0, 0], 
-            [0, 0, 0, 0], 
-            [0, 0, 1, 0], 
-            [0, 1, 1, 1]
-            ])
-class S:
+        self.matrix = np.array([
+            [0, 1, 0], 
+            [1, 1, 1]
+        ])
+
+class S(Piece):
     def __init__(self):
-        self.grid = np.array([
-            [0, 0, 0, 0], 
-            [0, 0, 0, 0], 
-            [0, 0, 1, 1], 
-            [0, 1, 1, 0]
-            ])
-class Z:
+        self.matrix = np.array([
+            [0, 1, 1], 
+            [1, 1, 0]
+        ])
+
+class Z(Piece):
     def __init__(self):
-        self.grid = np.array([
-            [0, 0, 0, 0], 
-            [0, 0, 0, 0], 
-            [0, 1, 1, 0], 
-            [0, 0, 1, 1]
-            ])
-class J:
+        self.matrix = np.array([
+            [1, 1, 0], 
+            [0, 1, 1]
+        ])
+
+class J(Piece):
     def __init__(self):
-        self.grid = np.array([
-            [0, 0, 0, 0], 
-            [0, 0, 1, 0], 
-            [0, 0, 1, 0], 
-            [0, 1, 1, 0]
-            ])
-class L:
+        self.matrix = np.array([
+            [1, 0, 0], 
+            [1, 1, 1]
+        ])
+
+class L(Piece):
     def __init__(self):
-        self.grid = np.array([
-            [0, 0, 0, 0], 
-            [0, 1, 0, 0], 
-            [0, 1, 0, 0], 
-            [0, 1, 1, 0]
-            ])
+        self.matrix = np.array([
+            [0, 0, 1], 
+            [1, 1, 1]
+        ])
