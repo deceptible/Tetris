723827fc8377ce2256ca17ca7e93c4956b1ac51b
deceptible <mitchellfreitas19@gmail.com>
2026-05-12 17:54:43 -0400

    addPiece

--- GameBoard.py
+++ GameBoard.py
@@ -1,4 +1,5 @@
 import numpy as np
+import Piece
 
 class GameBoard: 
     def __init__(self):
@@ -9,3 +10,17 @@ class GameBoard:
 
     def getBoard(self):
         return self.currentBoard
+    
+    def addPiece(self, Piece):
+        newCoords = []
+
+        for c in Piece.getCoordinates():
+            r = c[1]
+            c = c[0]
+            if self.currentBoard[(r + 1,c + 4)] == (2):
+                print("Error: Location Already in Use")
+            else:
+                self.currentBoard[(r + 1,c + 4)] = (2)
+                newCoords.append((r + 1,c + 4))
+
+        Piece.setCoordinates(newCoords)
--- Piece.py
+++ Piece.py
@@ -2,21 +2,41 @@ import numpy as np
 
 class Piece:
     def Transpose(self):
+        for coords in range(len(self.coordinates)):
+            r = self.coordinates[coords][0]
+            c = self.coordinates[coords][1]
+            self.coordinates[coords] = (c,r)
+
         return np.transpose(self.matrix)
 
     def Rotate(self):
         transposed = self.Transpose()
 
+        columns = transposed.shape[1]
+        print(columns)
+
+        for coords in range(len(self.coordinates)):
+            r = self.coordinates[coords][0]
+            c = self.coordinates[coords][1]
+            self.coordinates[coords] = (r,(columns - 1) - c)
+
         self.matrix = np.fliplr(transposed)
     
     def getMatrix(self):
         return self.matrix
+    
+    def getCoordinates(self):
+        return self.coordinates
+    
+    def setCoordinates(self, coords):
+        self.coordinates = coords
 
 class I(Piece):
     def __init__(self):
         self.matrix = np.array([
             [1, 1, 1, 1]
         ])
+        self.coordinates = [(0,0), (0,1), (0,2), (0,3)]
 
 class O(Piece):
     def __init__(self):
@@ -24,6 +44,7 @@ class O(Piece):
             [1, 1], 
             [1, 1]
         ])
+        self.coordinates = [(0,0), (0,1), (1,0), (1,1)]
 
 class T(Piece):
     def __init__(self):
@@ -31,6 +52,7 @@ class T(Piece):
             [0, 1, 0], 
             [1, 1, 1]
         ])
+        self.coordinates = [(0,1), (1,0), (1,1), (1,2)]
 
 class S(Piece):
     def __init__(self):
@@ -38,6 +60,7 @@ class S(Piece):
             [0, 1, 1], 
             [1, 1, 0]
         ])
+        self.coordinates = [(0,1), (0,2), (1,0), (1,1)]
 
 class Z(Piece):
     def __init__(self):
@@ -45,6 +68,7 @@ class Z(Piece):
             [1, 1, 0], 
             [0, 1, 1]
         ])
+        self.coordinates = [(0,0), (0,1), (1,1), (1,2)]
 
 class J(Piece):
     def __init__(self):
@@ -52,6 +76,7 @@ class J(Piece):
             [1, 0, 0], 
             [1, 1, 1]
         ])
+        self.coordinates = [(0,0), (1,0), (1,1), (1,2)]
 
 class L(Piece):
     def __init__(self):
@@ -59,3 +84,4 @@ class L(Piece):
             [0, 0, 1], 
             [1, 1, 1]
         ])
+        self.coordinates = [(0,2), (1,0), (1,1), (1,2)]
